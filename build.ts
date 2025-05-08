f28edc46b1aeb51e72b100164a02d686b2c6fc39
AryaveerSR <51504825+AryaveerSR@users.noreply.github.com>
2025-05-08 14:47:51 +0530

    fix path joining in build script

--- build.ts
+++ build.ts
@@ -1,4 +1,5 @@
 import { Glob } from "bun";
+import { join } from "path";
 
 await Bun.build({
   entrypoints: ["src/index.html"],
@@ -16,4 +17,6 @@ const files = await Array.fromAsync(new Glob("assets/*").scan("src"));
 files.push("googlee7932333831ee963.html");
 files.push("favicon.ico");
 
-files.forEach((file) => Bun.write("dist\\" + file, Bun.file("src\\" + file)));
+files.forEach((file) => {
+  Bun.write(join("dist", file), Bun.file(join("src", file)));
+});
