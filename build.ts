24595422a7d9d9a83d25c392b24915ccd01cb36f
AryaveerSR <51504825+AryaveerSR@users.noreply.github.com>
2025-05-10 00:25:38 +0530

    remove google html

--- build.ts
+++ build.ts
@@ -14,9 +14,6 @@ await Bun.build({
 
 const files = await Array.fromAsync(new Glob("assets/*").scan("src"));
 
-files.push("googlee7932333831ee963.html");
-files.push("favicon.ico");
-
 files.forEach((file) => {
   Bun.write(join("dist", file), Bun.file(join("src", file)));
 });
